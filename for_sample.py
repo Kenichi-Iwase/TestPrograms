9ac6a38245aadf59c1671bf596c2bb359c0b7002
Kenichi-Iwase <kenichi.iwasse@gmail.com>
2021-03-07 12:27:45 +0900

    update for_sample.py to add new code

--- for_sample.py
+++ for_sample.py
@@ -1,2 +1,21 @@
 for i in range(10):
   print('Hello, world!')
+
+name = "Ted"
+for character in name:
+  print(character)
+
+shows = ["GOT", "Narcos", "Vice"]
+for show in shows:
+  print(show)
+
+movies = ["ウォーキング・デッド", "アントラージュ", "ザ・ソプラノズ", "ヴァンパイア・ダイアリーズ"]
+for movie in movies:
+  print(movie)
+
+for i in range(25,51):
+  print(i)
+
+for i, movie in enumerate(movies):
+  result = "No.{} Title:{}".format(i, movie)
+  print(result)
