7b3382a540f849b8f1d60f3520668f08f4755ff9
Kenichi-Iwase <kenichi.iwasse@gmail.com>
2021-03-07 12:44:53 +0900

    commit R and JavaScript

---
+++ FunctionTest01.R
@@ -0,0 +1,29 @@
+myfunc01 <- function() {
+	return(1)
+}
+
+myfunc02 <- function(x) {
+	return(x)
+}
+
+mydouble <- function(x) {
+	return(2*x)
+}
+
+mytimes <- function(x, y) {
+	return(x*y)
+}
+
+mypower01 <- function(x) {
+	return(x^2)
+}
+
+mypower02 <- function(x, y) {
+	return(x^y)
+}
+
+mysqrtlog <- function(x) {
+	y <- sqrt(x)
+	z <- log(y)
+	return(z)
+}
