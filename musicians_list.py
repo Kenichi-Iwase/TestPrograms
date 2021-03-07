941ad2170d7c47830b43162ba04be72e800ded07
Kenichi-Iwase <kenichi.iwasse@gmail.com>
2021-03-07 23:09:04 +0900

    add musicians

--- musicians_list.py
+++ musicians_list.py
@@ -1,5 +1,8 @@
 # musician list sample script
-musicians = ['Deep Purple', 'Bon Jovi', 'Bud Pauwell']
+musicians = [
+    'Deep Purple', 'Bon Jovi', 'Bud Pauwell', 'Mr.Children',
+    'Zard', 'LiSA', 'あいみょん', 'Princes Princes'
+    ]
 print(musicians[0])
 print(musicians[1])
 print(musicians)
