82df753f289cb1c01a59f5a92fb90ac568db0ca4
Kenichi-Iwase <kenichi.iwasse@gmail.com>
2021-03-09 12:09:08 +0900

    Add Python folder and if script sample

---
+++ Python/if_sample.py
@@ -0,0 +1,11 @@
+def greeting(home):
+    if home == "アメリカ":
+        print("Hello, America!")
+    elif home == "日本":
+        print("Hello, Japan!")
+    else:
+        print("Hello, world!")
+
+greeting("アメリカ")
+greeting("日本")
+greeting("ロシア")
