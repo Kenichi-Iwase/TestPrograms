affd9e92d9aa1b44331f85867cc0ec24067d624e
Kenichi-Iwase <kenichi.iwasse@gmail.com>
2021-03-11 23:48:17 +0900

    Delete some Python scripts

--- Python/write_test.py
+++
@@ -1,4 +0,0 @@
-st = open("st.txt", "w")
-st.write("Hi from Python!\n")
-st.write("I have a dream.")
-st.close()
--- Python/write_test_jp.py
+++
@@ -1,3 +0,0 @@
-st = open("st_jp.txt", "w", encoding="utf-8")
-st.write("Pythonからこんにちは！")
-st.close()
