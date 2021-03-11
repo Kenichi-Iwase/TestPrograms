23e63ec2bd0d4a504d25020e8f16c81ee9c6c80f
Kenichi-Iwase <kenichi.iwasse@gmail.com>
2021-03-12 00:09:29 +0900

    Modify CSV sample script

--- Python/csv_sample.py
+++ Python/csv_sample.py
@@ -5,7 +5,9 @@ with open("st.csv", "w", newline="") as f:
     w = csv.writer(f, delimiter=",")
     w.writerow(["one", "two", "three"])
     w.writerow(["four", "five", "six"])
+    w.writerow(["seven", "eight", "nine"])
 
+# read CSV file
 with open("st.csv", "r") as f:
     r = csv.reader(f, delimiter=",")
     for row in r:
