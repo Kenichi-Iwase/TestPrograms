3e747cd22670dc646c4d034df1f94182cf53d764
Kenichi-Iwase <kenichi.iwasse@gmail.com>
2021-07-23 23:32:46 +0900

    Add method play script by Python

---
+++ Python/DataStructure/method_play.py
@@ -0,0 +1,37 @@
+# play 1
+x1 = 'Hello, world!'
+print(x1.upper())
+print(x1.lower())
+print(x1.capitalize())
+print(x1.replace('o', '@'))
+
+# play 2
+x2 = 'I have a pen.'
+print(x2.upper())
+print(x2.lower())
+print(x2.capitalize())
+print(x2.replace(' ', '@'))
+
+# play 3
+x3 = ['apple', 'orange', 'pear', 'melon']
+for x in x3:
+    print(x.upper())
+    print(x.lower())
+    print(x.capitalize())
+    print(x.replace('a', 'e'))
+
+# play 4
+x4 = {'apple', 'orange', 'pear', 'melon'}
+for x in x4:
+    print(x.upper())
+    print(x.lower())
+    print(x.capitalize())
+    print(x.replace('a', 'e'))
+
+# play 5
+x5 = ('apple', 'orange', 'pear', 'melon')
+for x in x5:
+    print(x.upper())
+    print(x.lower())
+    print(x.capitalize())
+    print(x.replace('l', 'r'))
