6601ce4a8faf68cd75cdeb8f450b46a4c96527bc
Kenichi-Iwase <kenichi.iwasse@gmail.com>
2021-08-23 23:35:01 +0900

    Modify weight list in August

--- MATLAB_Octave/WeightTable/draw_weight_series.m
+++ MATLAB_Octave/WeightTable/draw_weight_series.m
@@ -1,5 +1,16 @@
-T = readtable('WeightListAug.csv');
+% CSVファイル読み込み
+T = readtable('WeightListAug2021.csv');
+
+% 体重変化グラフを表示
 plot(T.Date, T.Weight)
 title('体重変化グラフ　8月')
 xlabel('日付')
 ylabel('体重[kg]')
+saveas(gcf, 'WeightGraph.png')
+
+% 体重と体脂肪率を散布図で表示
+scatter(T.Weight, T.WeightRatio)
+title('体重と体脂肪率の関係')
+xlabel('体重[kg]')
+ylabel('体脂肪率[%]')
+saveas(gcf, 'WeightRatio.png')
