8bb7d56c03c9ba2eb4e794035481c4d34cd7115d
Kenichi-Iwase <kenichi.iwasse@gmail.com>
2021-08-29 12:12:22 +0900

    Add weight data

--- MATLAB_Octave/WeightTable/draw_weight_series.m
+++ MATLAB_Octave/WeightTable/draw_weight_series.m
@@ -6,14 +6,17 @@ plot(T.Date, T.Weight)
 title('体重変化グラフ　2021年')
 xlabel('日付')
 ylabel('体重[kg]')
-saveas(gcf, 'WeightGraph.png')
+saveas(gcf, 'WeightGraphSeries.png')
 
 % 体重と体脂肪率を散布図で表示
 scatter(T.Weight, T.WeightRatio)
 title('体重と体脂肪率の関係')
 xlabel('体重[kg]')
 ylabel('体脂肪率[%]')
-saveas(gcf, 'WeightRatio.png')
+saveas(gcf, 'WeightRatioScatter.png')
+
+% 体重と体脂肪率の相関係数を求める
+corrcoef(T.Weight, T.WeightRatio)
 
 % 体重のヒストグラムを表示
 histogram(T.Weight)
